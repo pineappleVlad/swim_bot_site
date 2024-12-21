d6a3106b94900b3514f2a654d85fb2b22720d9bb
pineappleVlad <vlad.shumilo.2014@mail.ru>
2024-12-22 00:00:23 +0700

    fix

--- swim_bot/database/db_connection.py
+++ swim_bot/database/db_connection.py
@@ -1,20 +1,9 @@
 from config import DB_HOST, DB_NAME, DB_PASSWORD, DB_USERNAME, DB_PORT
 import asyncpg
 
-# async def connect_to_db(): # --- production
-#     db_params = {
-#         'host': "db",
-#         'database': DB_NAME,
-#         'user': DB_USERNAME,
-#         'password': DB_PASSWORD,
-#         'port': DB_PORT
-#     }
-#     connection = await asyncpg.connect(**db_params)
-#     return connection
-
-async def connect_to_db(): # --- develop
+async def connect_to_db(): # --- production
     db_params = {
-        'host': DB_HOST,
+        'host': "db",
         'database': DB_NAME,
         'user': DB_USERNAME,
         'password': DB_PASSWORD,
@@ -22,8 +11,19 @@ async def connect_to_db(): # --- develop
     }
     connection = await asyncpg.connect(**db_params)
     return connection
-async def close_db(connection):
-    await connection.close()
+
+# async def connect_to_db(): # --- develop
+#     db_params = {
+#         'host': DB_HOST,
+#         'database': DB_NAME,
+#         'user': DB_USERNAME,
+#         'password': DB_PASSWORD,
+#         'port': DB_PORT
+#     }
+#     connection = await asyncpg.connect(**db_params)
+#     return connection
+# async def close_db(connection):
+#     await connection.close()
 
 async def execute_query(query, params=None):
     connection = await connect_to_db()
